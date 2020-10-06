dedf831d4c9a4e7623f0b53c2e891e8a8c0d2619
krdormer <kevin.dormer18@yahoo.com>
2020-10-06 13:11:40 -0500

    Updates

---
+++ precache-manifest.cf883eabe426cb6d21dad87ddbad5ad0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b451f57764b5386eb60aa6f3f9bbbd8",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "fbd8723aab59fafe5feb",
+    "url": "/monsters-rolodex/static/css/main.988d399a.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fbd8723aab59fafe5feb",
+    "url": "/monsters-rolodex/static/js/main.e24702e2.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
